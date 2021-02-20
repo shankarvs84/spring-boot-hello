7b69c72b02d680cb51e72561eb68e1cd100973f8
shankarvs84 <40135754+shankarvs84@users.noreply.github.com>
2021-02-20 19:50:14 +0530

    inital commit

---
+++ src/main/java/com/example/springboothello/SpringBootHelloApp.java
@@ -0,0 +1,14 @@
+package com.example.springboothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloApp {
+   
+    	public static void main(String[] args) {
+    		SpringApplication.run(SpringBootHelloApplication.class, args);
+    	}
+
+
+}
